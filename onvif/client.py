980b57931903d44d19a8c7ea70bb617937178d51
James Adams <monocongo@gmail.com>
2019-02-13 15:40:11 -0500

    added a notification service in order to facilitate requesting a notifications subscription
    #37

--- onvif/client.py
+++ onvif/client.py
@@ -364,3 +364,6 @@ class ONVIFCamera(object):
 
     def create_receiver_service(self, from_template=True):
         return self.create_onvif_service('receiver', from_template)
+
+    def create_notification_service(self, from_template=True):
+        return self.create_onvif_service('notifications', from_template)
